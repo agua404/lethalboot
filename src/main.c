1417eb4961e837a3fdaacc2c6e37568dbd0c0b26
agua404 <johnpork@dontpickup.com>
2026-06-09 10:39:16 -0500

    new code, needs wprintf fixs

--- src/main.c
+++ src/main.c
@@ -1,18 +1,48 @@
-#include <wchar.h>
 #include <locale.h>
+#include <stdio.h>
+#include <wchar.h>
 
-int main(void)
-{
+int main(void) {
   setlocale(LC_ALL, "");
 
-  wchar_t message[2][5] = {
-      L"╔═╗",
-      L"╚═╝"
-  };
+  int X = 0;
+  int Y = 0;
 
-  for (int i = 0; i < 2; i++) {
-      wprintf(L"%ls\n", message[i]);
+  //  wchar_t message[2][5] = {
+  //      L"╔═╗",
+  //      L"╚═╝"
+  //  };
+
+  printf("Size X: ");
+  scanf("%d", &X);
+  printf("Size Y: ");
+  scanf("%d", &Y);
+
+  X = X - 2;
+  Y = Y - 2;
+
+  wprintf(L"╔");
+  for (int i = 0; i < X; i++) {
+    printf(" ");
   }
+  wprintf(L"╗");
+  printf("\n");
+
+  for (int i = 0; i < Y; i++) {
+    printf("l");
+    for (int i2 = 0; i2 < X; i2++) {
+      printf(" ");
+    }
+    printf("r");
+    printf("\n");
+  }
+
+  wprintf(L"%ls","╚");
+  for (int i = 0; i < X; i++) {
+    printf(" ");
+  }
+  wprintf(L"%ls", "╝");
+  printf("\n");
 
   return 0;
 }
