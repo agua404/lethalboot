cd90eb64e001627aaae113967f301c1c3a61189a
agua404 <johnpork@dontpickup.com>
2026-06-13 14:59:26 -0500

    working kinda gen

--- src/main.c
+++ src/main.c
@@ -1,17 +1,11 @@
 #include <locale.h>
 #include <stdio.h>
-#include <wchar.h>
 
-int main(void) {
+int main() {
   setlocale(LC_ALL, "");
 
   int X = 0;
   int Y = 0;
-
-  //  wchar_t message[2][5] = {
-  //      L"╔═╗",
-  //      L"╚═╝"
-  //  };
 
   printf("Size X: ");
   scanf("%d", &X);
@@ -19,29 +13,28 @@ int main(void) {
   scanf("%d", &Y);
 
   X = X - 2;
-  Y = Y - 2;
+  Y = (Y - 2) / 2;
 
-  wprintf(L"╔");
+  printf("┌");
   for (int i = 0; i < X; i++) {
-    printf(" ");
+    printf("─");
   }
-  wprintf(L"╗");
-  printf("\n");
+  printf("┐\n");
 
   for (int i = 0; i < Y; i++) {
-    printf("l");
+    printf("│");
     for (int i2 = 0; i2 < X; i2++) {
       printf(" ");
     }
-    printf("r");
+    printf("│");
     printf("\n");
   }
 
-  wprintf(L"%ls","╚");
+  printf("└");
   for (int i = 0; i < X; i++) {
-    printf(" ");
+    printf("─");
   }
-  wprintf(L"%ls", "╝");
+  printf("┘");
   printf("\n");
 
   return 0;
